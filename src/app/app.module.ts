dfaeb34e38deaef274b6be605dce5462dd95e4c9
Siddharthmishraweb <mishrasiddharth1999@gmail.com>
2024-07-13 07:57:46 +0530

    Updated FB changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,3 @@
-// import { NgModule } from '@angular/core';
-// import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-// import { CommonService } from './common.service';
-// import { AppRoutingModule } from './app-routing.module';
-// import { AppComponent } from './app.component';
-// import { DashboardComponent } from './dashboard/dashboard.component';
-// import { HttpClientModule } from '@angular/common/http';
-// import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
-// import { FormsModule } from '@angular/forms';
-
-// @NgModule({
-//   declarations: [
-//     AppComponent,
-//     DashboardComponent
-//   ],
-//   imports: [
-//     BrowserModule,
-//     AppRoutingModule,
-//     HttpClientModule,
-//     NgbModule,
-//     FormsModule
-//   ],
-//   providers: [
-//     provideClientHydration(),
-//     CommonService
-//   ],
-//   bootstrap: [AppComponent]
-// })
-// export class AppModule { }
-
 import { NgModule } from '@angular/core';
 import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
 import { CommonService } from './common.service';
@@ -37,12 +7,9 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { FormsModule } from '@angular/forms';
-import { SocialLoginModule, SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
-import { GoogleLoginProvider } from '@abacritt/angularx-social-login';
+import { SocialLoginModule, SocialAuthServiceConfig, FacebookLoginProvider, GoogleLoginProvider } from '@abacritt/angularx-social-login';
 import { LoginModalComponent } from './login-modal/login-modal.component';
-import { SocialAuthService, SocialUser } from '@abacritt/angularx-social-login';
 import { CreatePostComponent } from './create-post/create-post.component';
-
 
 @NgModule({
   declarations: [
@@ -70,6 +37,10 @@ import { CreatePostComponent } from './create-post/create-post.component';
           {
             id: GoogleLoginProvider.PROVIDER_ID,
             provider: new GoogleLoginProvider('1088154961211-oi9bnlc9hg2v0d6hc6qu2i4eielgcai4.apps.googleusercontent.com')
+          },
+          {
+            id: FacebookLoginProvider.PROVIDER_ID,
+            provider: new FacebookLoginProvider('854240906261396')
           }
         ]
       } as SocialAuthServiceConfig,
